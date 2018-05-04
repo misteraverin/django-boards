9c6c85d60668d7bf820f7ae08d3da4ad5d9ee2bb
Averin Maxim <amax0703@gmail.com>
2018-05-04 20:35:37 +0300

    Add new_topic, topics view

--- djangoboards/djangoboards/urls.py
+++ djangoboards/djangoboards/urls.py
@@ -21,5 +21,7 @@ from boards import views
 
 urlpatterns = [
     re_path(r'^$', views.home, name='home'),
+    re_path(r'^boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
+    re_path(r'^boards/(?P<pk>\d+)/new/$', views.new_topic, name='new_topic'),
     re_path(r'^admin/', admin.site.urls),
 ]
